d87d8639144cdf30c0ecc32521224425b6637050
Alex Peck <alex@alxpck.com>
2018-01-10 11:09:53 -0500

    lessons on const and let

--- js/person.js
+++ js/person.js
@@ -12,6 +12,9 @@ function personDescription(person) {
   // var description = person.firstName;
   // let is mutable so you can add to the description
   // let also has block level scoping
+  // block level scoping means the scope of a variable
+  // between curly braces... {} < a block is the stuff
+  // within the curly braces
   let description = person.firstName;
   if (person.role) {
     description += ' is a ' + person.role;
